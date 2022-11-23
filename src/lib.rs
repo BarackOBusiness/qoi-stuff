4703c3e7964d90537170454ac1b192c2eb5f9cd3
The-head-obamid <nichjoh21@gmail.com>
2022-11-22 20:53:55 -0500

    non descriptive commit #2, because I don't know what I'm doing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+pub enum Channels {
+    RGB = 3,
+    RGBA = 4,
+}
+
+pub enum ColorSpace {
+    SRGB = 0,
+    Linear = 1,
+}
+
 struct QoiHeader {
     width: u32,
     height: u32,
@@ -6,5 +16,7 @@ struct QoiHeader {
 }
 
 impl QoiHeader {
-    
+    pub fn qoi_write(filename: char) {
+        
+    }
 }
