46e10a6a0c1d77b843938a8edc88537e2df7bb88
The-head-obamid <nichjoh21@gmail.com>
2022-11-22 21:54:07 -0500

    Placeholder main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    
+    let nine = 9;
+    let ten = 12;
+    
+    if nine + ten == 21 {
+        println!("21");
+    } else {
+        println!("What the fuck man");
+    }
 }
